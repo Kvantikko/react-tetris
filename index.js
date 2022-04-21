4041241a44c5a97e53657580001c0664129b9dd4
Mikko Syrjälä <mijsyr@utu.fi>
2022-04-21 21:29:10 +0300

    Database connection established

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-
 const app = express()
+const Score = require('./models/score')
 
 app.use(express.static('build'))
 app.use(express.json())
@@ -18,25 +19,16 @@ app.get('/', (req, res) => {
  * Sends all the scores to client
  */
 app.get('/api/scores', (reqquest, response) => {
+    Score.find({}).then(scores => {
+        response.json(scores)
+      })
+    /*
     if (scores) {
         response.json(scores)
     } else {
         response.status(404).end()
     } 
-})
-
-/**
- * Sends a specific score entry by id to client
- */
-app.get('/api/scores/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const score = scores.find(note => note.id === id)
-
-    if (score) {
-        response.json(score)
-    } else {
-        response.status(404).end()
-    }
+    */
 })
 
 /**
@@ -45,6 +37,20 @@ app.get('/api/scores/:id', (request, response) => {
 app.post('/api/scores', (request, response) => {
     const body = request.body
     
+    const score = new Score({
+        name: body.name,
+        score: body.score,
+    })
+
+    score
+        .save()
+        .then(savedScore => savedScore.toJSON())
+        .then(savedAndFormattedScore => {
+            response.json(savedAndFormattedScore)
+        })
+        .catch(error => console.log(error))
+
+    /*
     if (!body.name) {
         return response.status(400).json({ 
             error: 'missing name' 
@@ -60,12 +66,13 @@ app.post('/api/scores', (request, response) => {
     scores = scores.concat(score)
 
     response.json(score)
+    */
 })
 
 /**
  * Dummy data before database connection implementation. If database is not implemented
  * before the project deadline, will use this array as the "database".
- */
+ 
 let scores = [
     {
         name: "Mikko",
@@ -80,16 +87,14 @@ let scores = [
     
 ]
 
-/**
- * Generates id for a new score entry
- */
+
 const generateId = () => {
     const maxId = scores.length > 0
       ? Math.max(...scores.map(n => n.id))
       : 0
     return maxId + 1
 }
-
+*/
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
